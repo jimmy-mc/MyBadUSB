5a70364f8cd1ba59b2a17cb30373cec3f20a7e31
jimmy-mc <james.mcmillan@gmail.com>
2025-05-12 16:18:58 +0100

    Update lockitup-wallpaper.cmd

--- lockitup-wallpaper.cmd
+++ lockitup-wallpaper.cmd
@@ -1,18 +1,26 @@
 @echo off
 setlocal
+
+:: Set variables
 set "img=%TEMP%\lockitup.jpg"
+set "self=%~f0"
+
+:: Download wallpaper
 curl -s -o "%img%" "https://raw.githubusercontent.com/jimmy-mc/MyBadUSB/main/lockitup-wallpaper.jpg" >nul 2>&1
 
+:: Set wallpaper if download succeeded
 if exist "%img%" (
     reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f >nul 2>&1
     RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 )
 
-:: Self-delete using a new cmd.exe instance (no "batch file not found" error)
+:: Self-delete using a temp script
+set "delcmd=%TEMP%\_delme.cmd"
 (
     echo @echo off
-    echo ping -n 3 127.0.0.1 >nul
-    echo del "%%~f0"
-) > "%TEMP%\_delme.cmd"
-start "" /min "%TEMP%\_delme.cmd"
+    echo ping -n 3 127.0.0.1 ^>nul
+    echo del "%self%"
+) > "%delcmd%"
+start "" /min "%delcmd%"
+
 exit
