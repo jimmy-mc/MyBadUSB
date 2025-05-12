c1417cd6a7ba2090ea818b49ca4252e67f4ed1b2
jimmy-mc <james.mcmillan@gmail.com>
2025-05-12 16:10:07 +0100

    Update lockitup-wallpaper.cmd

--- lockitup-wallpaper.cmd
+++ lockitup-wallpaper.cmd
@@ -1,5 +1,14 @@
 @echo off
+setlocal
 set "img=%TEMP%\lockitup.jpg"
+echo Downloading image to: %img%
 curl -o "%img%" "https://raw.githubusercontent.com/jimmy-mc/MyBadUSB/main/lockitup-wallpaper.jpg"
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+if exist "%img%" (
+    echo Image downloaded successfully.
+    reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f
+    RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+) else (
+    echo Failed to download image.
+)
+endlocal
