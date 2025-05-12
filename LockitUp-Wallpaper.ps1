54836bd44d55a7085c9123de2d67c3e73da9d7b3
jimmy-mc <james.mcmillan@gmail.com>
2025-05-12 08:36:48 +0100

    Create LockitUp-Wallpaper.ps1

---
+++ LockitUp-Wallpaper.ps1
@@ -0,0 +1,18 @@
+$imgUrl = "https://example.com/image.jpg"
+$wallpaperPath = "$env:USERPROFILE\LockItUp-Wallpaper.jpg"
+
+Invoke-WebRequest -Uri $imgUrl -OutFile $wallpaperPath
+
+Add-Type @"
+using System.Runtime.InteropServices;
+public class Wallpaper {
+  [DllImport("user32.dll", SetLastError = true)]
+  public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+
+$SPI_SETDESKWALLPAPER = 20
+$SPIF_UPDATEINIFILE = 1
+$SPIF_SENDWININICHANGE = 2
+
+[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $wallpaperPath, $SPIF_UPDATEINIFILE -bor $SPIF_SENDWININICHANGE)
