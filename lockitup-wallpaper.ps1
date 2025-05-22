adef778ebd2be4a719894d287f7080e4b1b5f630
jimmy-mc <james.mcmillan@gmail.com>
2025-05-22 20:29:34 +0100

    Update lockitup-wallpaper.ps1

--- lockitup-wallpaper.ps1
+++ lockitup-wallpaper.ps1
@@ -1,18 +1,11 @@
-$imgUrl = "https://raw.githubusercontent.com/jimmy-mc/MyBadUSB/main/lockitup-wallpaper.jpg"
-$wallpaperPath = "$env:USERPROFILE\lockitup-wallpaper.jpg"
-
-Invoke-WebRequest -Uri $imgUrl -OutFile $wallpaperPath
-
-Add-Type @"
-using System.Runtime.InteropServices;
-public class Wallpaper {
-  [DllImport("user32.dll", SetLastError = true)]
-  public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-}
-"@
-
-$SPI_SETDESKWALLPAPER = 20
-$SPIF_UPDATEINIFILE = 1
-$SPIF_SENDWININICHANGE = 2
-
-[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $wallpaperPath, $SPIF_UPDATEINIFILE -bor $SPIF_SENDWININICHANGE)
+# Change the wallpaper
+if ($wp) {
+    try {
+        $wpPath = "$env:TEMP\wallpaper.jpg"
+        Invoke-WebRequest -Uri $wp -OutFile $wpPath -UseBasicParsing
+        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name Wallpaper -Value $wpPath
+        rundll32.exe user32.dll,UpdatePerUserSystemParameters
+    } catch {
+        Write-Warning "Failed to apply wallpaper: $_"
+    }
+} 
